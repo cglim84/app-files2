59564c53d1783d8bbcc923a71247b57892d14f86
cglim84 <cglim84@gmail.com>
2022-01-15 10:23:50 +0900

    initial files

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cam21_stitch_xhc",
+      "name": "cam21_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.06814248624676722,
+        "pitch": 0.02653372173639923,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6657946878960157,
+          "pitch": 0.17788436704129396,
+          "rotation": 0,
+          "target": "1-cam20_stitch_xhc"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cam20_stitch_xhc",
+      "name": "cam20_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -1.2711544558537877,
+        "pitch": 0.04510559956621485,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7555744900254737,
+          "pitch": 0.14000074546668806,
+          "rotation": 0,
+          "target": "0-cam21_stitch_xhc"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
